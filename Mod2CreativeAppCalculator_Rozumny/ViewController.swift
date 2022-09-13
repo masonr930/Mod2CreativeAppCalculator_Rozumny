60cad27e3c8002e32b12cde7d47ccc4534041c30
Mason <mason.rozumny23@d155.org>
2022-09-13 07:13:01 -0500

    Initial Commit

---
+++ Mod2CreativeAppCalculator_Rozumny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod2CreativeAppCalculator_Rozumny
+//
+//  Created by MASON ROZUMNY on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
